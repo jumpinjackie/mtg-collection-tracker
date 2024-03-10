81b1e4735130dc015a7880562de6a91d776d7453
Jackie Ng <jumpinjackie@gmail.com>
2024-03-10 23:43:45 +1100

    Add container and deck listing commands

---
+++ src/MtgCollectionTracker.Console/Commands/ListContainersCommand.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using ConsoleTables;
+using Microsoft.Extensions.DependencyInjection;
+using MtgCollectionTracker.Core.Model;
+using MtgCollectionTracker.Core.Services;
+
+namespace MtgCollectionTracker.Console.Commands;
+
+[Verb("list-containers")]
+internal class ListContainersCommand : CommandBase
+{
+    protected override async ValueTask<int> ExecuteInternalAsync(IServiceProvider serviceProvider)
+    {
+        var service = serviceProvider.GetRequiredService<CollectionTrackingService>();
+
+        var decks = service.GetContainers();
+        var table = new ConsoleTable(
+            nameof(ContainerSummaryModel.Id),
+            nameof(ContainerSummaryModel.Name),
+            nameof(ContainerSummaryModel.Total));
+
+        foreach (var m in decks)
+        {
+            table.AddRow(m.Id, m.Name, m.Total);
+        }
+
+        table.Write();
+
+        return 0;
+    }
+}
---
+++ src/MtgCollectionTracker.Console/Commands/ListDecksCommand.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine;
+using ConsoleTables;
+using Microsoft.Extensions.DependencyInjection;
+using MtgCollectionTracker.Core.Model;
+using MtgCollectionTracker.Core.Services;
+
+namespace MtgCollectionTracker.Console.Commands;
+
+[Verb("list-decks")]
+internal class ListDecksCommand : CommandBase
+{
+    protected override async ValueTask<int> ExecuteInternalAsync(IServiceProvider serviceProvider)
+    {
+        var service = serviceProvider.GetRequiredService<CollectionTrackingService>();
+
+        var decks = service.GetDecks();
+        var table = new ConsoleTable(
+            nameof(DeckSummaryModel.Id),
+            nameof(DeckSummaryModel.Name),
+            nameof(DeckSummaryModel.ContainerName),
+            nameof(DeckSummaryModel.MaindeckTotal),
+            nameof(DeckSummaryModel.SideboardTotal));
+
+        foreach (var m in decks)
+        {
+            table.AddRow(m.Id, m.Name, m.ContainerName, m.MaindeckTotal, m.SideboardTotal);
+        }
+
+        table.Write();
+
+        return 0;
+    }
+}
