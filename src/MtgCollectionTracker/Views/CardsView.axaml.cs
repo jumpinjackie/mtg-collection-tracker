9d5fd425992fff3422adff9255ad32538c5c36fd
Jackie Ng <jumpinjackie@gmail.com>
2024-03-23 00:58:58 +1100

    Allow search on enter keypress

--- src/MtgCollectionTracker/Views/CardsView.axaml.cs
+++ src/MtgCollectionTracker/Views/CardsView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using MtgCollectionTracker.ViewModels;
 
 namespace MtgCollectionTracker.Views;
 
@@ -7,5 +9,13 @@ public partial class CardsView : UserControl
     public CardsView()
     {
         InitializeComponent();
+    }
+
+    private void OnSearchKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && this.DataContext is CardsViewModel vm)
+        {
+            vm.PerformSearchCommand.Execute(null);
+        }
     }
 }
