05473d43c643aafa262a0bba6f680e2be741e928
Jackie Ng <jumpinjackie@gmail.com>
2025-08-01 23:30:45 +1000

    Try to fix up browser project

--- src/MtgCollectionTracker.Browser/Program.cs
+++ src/MtgCollectionTracker.Browser/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System.Runtime.Versioning;
 using System.Threading.Tasks;
-
 using Avalonia;
 using Avalonia.Browser;
-
 using MtgCollectionTracker;
 
 [assembly: SupportedOSPlatform("browser")]
 
 internal partial class Program
 {
-    private static async Task Main(string[] args) => await BuildAvaloniaApp()
+    private static Task Main(string[] args) => BuildAvaloniaApp()
             .WithInterFont()
             .StartBrowserAppAsync("out");
 
