1345cda647ccbb2a0cfd92d6f34cb8cb0aee0c0d
Jackie Ng <jumpinjackie@gmail.com>
2026-03-11 01:34:35 +1100

    Fix post-merge build

--- src/MtgCollectionTracker/Services/Messaging/ContainerAndDeckMessages.cs
+++ src/MtgCollectionTracker/Services/Messaging/ContainerAndDeckMessages.cs
@@ -14,7 +14,7 @@ internal record CardsRemovedFromDeckMessage(int? DeckId, List<Guid> SkuIds);
 
 internal record CardsSentToDeckMessage(int DeckId, string DeckName, List<Guid> SkuIds);
 
-internal record DeckSideboardChangedMessage(int DeckId, List<int> SkuIds, bool IsSideboard);
+internal record DeckSideboardChangedMessage(int DeckId, List<Guid> SkuIds, bool IsSideboard);
 
 internal record DeckTotalsChangedMessage(List<int> DeckIds);
 
